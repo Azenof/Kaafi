42da2192c3323861c01131b3ec8d053f989770f1
tanjim-ahemd <paladinsghost10@gmail.com>
2025-12-06 16:53:52 +0600

    fixed_bottom_nav_and_resize_dotlottie.

---
+++ lib/feature/screens/shop/account/widget/ButtonwithTitle.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/button/circulariconbutton.dart';
+
+class ButtonwithTitle extends StatelessWidget {
+  const ButtonwithTitle({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.onpressd,
+    required this.icon,
+  });
+  final String title;
+  final Color color;
+  final VoidCallback onpressd;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomIconButton(
+          minimumSize: const Size(50, 50),
+          size: 24,
+          iconColor: color,
+          icon: icon,
+          onPressed: onpressd,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            color: const Color(0xFF9E9E9E), // gray500 equivalent
+            fontSize: 12, // sm equivalent
+          ),
+        ),
+      ],
+    );
+  }
+}
+
---
+++ lib/feature/screens/shop/account/widget/IconListtile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/button/circulariconbutton.dart';
+
+class IconListTitle extends StatelessWidget {
+  const IconListTitle({
+    super.key,
+    this.icon = Icons.person_add_outlined,
+    required this.ontap,
+    this.title = "Old Accounts & Orders",
+    required this.color,
+  });
+  final IconData icon;
+  final VoidCallback ontap;
+  final String title;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: ontap,
+      leading: CustomIconButton(
+        icon: icon,
+        backgroundColor: color,
+        onPressed: () {},
+      ),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.w400)),
+    );
+  }
+}
